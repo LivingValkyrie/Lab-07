85667bbefda9d60182165a989220eff4052522a1
LivingValkyrie <deadwynn@gmail.com>
2015-10-20 21:49:49 -0500

    turn in
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

--- Multiplayer Lab 02/Assets/Scripts/Multiplayer/Lab02a_PlayerControl.cs
+++ Multiplayer Lab 02/Assets/Scripts/Multiplayer/Lab02a_PlayerControl.cs
@@ -1,15 +1,100 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
+using System.Security;
 
-public class Lab02a_PlayerControl : MonoBehaviour {
+public class Lab02a_PlayerControl : NetworkBehaviour {
+    #region Fields
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SyncVar] PlayerState state;
+
+    #endregion
+
+    // Use this for initialization
+    void Start() {
+        InitState();
+        SyncState();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isLocalPlayer) {
+            KeyCode[] possibleKeys = {KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.W, KeyCode.Q, KeyCode.E, KeyCode.Space};
+            foreach (KeyCode possibleKey in possibleKeys) {
+                //if ( !Input.GetKey( possibleKey ) ) {
+                //    continue;
+                //}
+                //CmdMoveOnServer( possibleKey );
+
+                if (Input.GetKey(possibleKey)) {
+                    CmdMoveOnServer(possibleKey);
+                }
+            }
+
+            SyncState();
+        }
+    }
+
+    [Command]
+    void CmdMoveOnServer(KeyCode pressedKey) {
+        state = Move(state, pressedKey);
+    }
+
+    PlayerState Move(PlayerState previous, KeyCode newKey) {
+        float deltaX = 0, deltaY = 0, deltaZ = 0;
+        float deltaRotationY = 0;
+
+        switch (newKey) {
+            case KeyCode.Q:
+                deltaX = -0.5f;
+                break;
+            case KeyCode.S:
+                deltaZ = -0.5f;
+                break;
+            case KeyCode.E:
+                deltaX = 0.5f;
+                break;
+            case KeyCode.W:
+                deltaZ = 0.5f;
+                break;
+            case KeyCode.A:
+                deltaRotationY = -1f;
+                break;
+            case KeyCode.D:
+                deltaRotationY = 1f;
+                break;
+        }
+
+        return new PlayerState {
+            posX = deltaX + previous.posX,
+            posY = deltaY + previous.posY,
+            posZ = deltaZ + previous.posZ,
+            rotX = previous.rotX,
+            rotY = deltaRotationY + previous.rotY,
+            rotZ = previous.rotZ
+        };
+    }
+
+    void SyncState() {
+        transform.position = new Vector3(state.posX, state.posY, state.posZ);
+        transform.rotation = Quaternion.Euler(state.rotX, state.rotY, state.rotZ);
+    }
+
+    [Server]
+    void InitState() {
+        state = new PlayerState {
+            posX = -119f,
+            posY = 165.08f,
+            posZ = -924f,
+            rotX = 0f,
+            rotY = 0f,
+            rotZ = 0f
+        };
+    }
+}
+
+public struct PlayerState {
+    public float posX, posY, posZ;
+    public float rotX, rotY, rotZ;
+    public int movementNumber;
 }
---
+++ Multiplayer Lab 02/Assets/Scripts/Multiplayer/Lab02b_PlayerControlPrediction.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: Lab02b_PlayerControlPrediction 
+/// </summary>
+public class Lab02b_PlayerControlPrediction : NetworkBehaviour {
+    #region Fields
+
+    [SyncVar(hook = "OnServerStateChanged")]
+    PlayerState serverState;
+    PlayerState predictedState;
+
+    Queue<KeyCode> pendingMoves; 
+
+    #endregion
+
+    // Use this for initialization
+    void Start() {
+        InitState();
+        predictedState = serverState;
+        if (isLocalPlayer) {
+            pendingMoves = new Queue<KeyCode>();
+            UpdatePredictedState();
+        }
+        SyncState();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if ( isLocalPlayer ) {
+            print("Pending moves: " + pendingMoves.Count);
+
+            KeyCode[] possibleKeys = { KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.W, KeyCode.Q, KeyCode.E, KeyCode.Space };
+            foreach ( KeyCode possibleKey in possibleKeys ) {
+                //if ( !Input.GetKey( possibleKey ) ) {
+                //    continue;
+                //}
+                //CmdMoveOnServer( possibleKey );
+
+                if ( Input.GetKey( possibleKey ) ) {
+                    CmdMoveOnServer( possibleKey );
+                }
+
+                pendingMoves.Enqueue(possibleKey);
+                UpdatePredictedState();
+                CmdMoveOnServer(possibleKey);
+            }
+
+        }
+
+        SyncState();
+       
+    }
+
+    [Command]
+    void CmdMoveOnServer( KeyCode pressedKey ) {
+        serverState = Move(serverState, pressedKey);
+    }
+
+    PlayerState Move( PlayerState previous, KeyCode newKey ) {
+        float deltaX = 0, deltaY = 0, deltaZ = 0;
+        float deltaRotationY = 0;
+
+        switch ( newKey ) {
+            case KeyCode.Q:
+                deltaX = -0.5f;
+                break;
+            case KeyCode.S:
+                deltaZ = -0.5f;
+                break;
+            case KeyCode.E:
+                deltaX = 0.5f;
+                break;
+            case KeyCode.W:
+                deltaZ = 0.5f;
+                break;
+            case KeyCode.A:
+                deltaRotationY = -1f;
+                break;
+            case KeyCode.D:
+                deltaRotationY = 1f;
+                break;
+        }
+
+        return new PlayerState {
+            movementNumber = 1 + previous.movementNumber,
+            posX = deltaX + previous.posX,
+            posY = deltaY + previous.posY,
+            posZ = deltaZ + previous.posZ,
+            rotX = previous.rotX,
+            rotY = deltaRotationY + previous.rotY,
+            rotZ = previous.rotZ
+        };
+    }
+
+    void SyncState() {
+        PlayerState stateToRender = isLocalPlayer ? predictedState : serverState;
+
+        transform.position = new Vector3(stateToRender.posX, stateToRender.posY, stateToRender.posZ);
+        transform.rotation = Quaternion.Euler(stateToRender.rotX, stateToRender.rotY, stateToRender.rotZ);
+    }
+
+    void OnServerStateChanged(PlayerState newState) {
+        serverState = newState;
+        if (pendingMoves != null) {
+            while (pendingMoves.Count > (predictedState.movementNumber - serverState.movementNumber)) {
+                pendingMoves.Dequeue();
+            }
+            UpdatePredictedState();
+        }
+    }
+
+    void UpdatePredictedState() {
+        predictedState = serverState;
+        foreach (KeyCode pendingMove in pendingMoves) {
+            predictedState = Move(predictedState, pendingMove);
+        }
+    }
+
+    [Server]
+    void InitState() {
+        serverState = new PlayerState {
+            posX = -119f,
+            posY = 165.08f,
+            posZ = -924f,
+            rotX = 0f,
+            rotY = 0f,
+            rotZ = 0f
+        };
+    }
+}
